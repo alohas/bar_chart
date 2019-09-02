666236a7a5c0721f442a2d32852f9573818675d9
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-09-02 13:37:25 +0200

    my own edition

--- chart.js
+++ chart.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const array = [];
+let i = 1;
+
+function getNumberOfCustomers() {
+  return Math.floor(Math.random() * 36);
+}
+
+function count() {
+  let queSize = getNumberOfCustomers();
+
+  if (array.length > 39) {
+    array.shift();
+    array.push(queSize);
+  } else {
+    array.push(queSize);
+  }
+
+  console.clear();
+  console.log(array);
+  if (i > 40) {
+    //console.log("stupid");
+
+    for (let j = 1; j < 41; j++) {
+      document.querySelector("#bar" + j).style.height = array[j - 1] * 4 + "px";
+      document.querySelector("#bar" + j).style.marginTop =
+        140 - array[j - 1] * 4 + "px";
+    }
+  } else {
+    document.querySelector("#bar" + i).style.height = queSize * 4 + "px";
+    document.querySelector("#bar" + i).style.marginTop =
+      140 - queSize * 4 + "px";
+    i++;
+  }
+}
+
+setInterval(count, 300);
